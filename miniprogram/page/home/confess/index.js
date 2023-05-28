d01109f0c4b854c3b7e541058fcea5241a3844fd
Eren <347561039@qq.com>
2023-05-29 00:25:18 +0800

    home page

---
+++ miniprogram/page/home/confess/index.js
@@ -0,0 +1,84 @@
+Page({
+  data: {
+    pagelist: [
+      {urls: [
+        'https://tvax1.sinaimg.cn/large/008tUvTqly1heek4qf0w9j31120kuwjd.jpg',
+        'https://tvax1.sinaimg.cn/large/008tUvTqgy1heeit6sccxj305k05kweg.jpg',
+        'https://tvax1.sinaimg.cn/large/008tUvTqgy1heeit6sccxj305k05kweg.jpg',
+        'https://tvax1.sinaimg.cn/large/008tUvTqgy1heeit6sccxj305k05kweg.jpg',
+        'https://tvax1.sinaimg.cn/large/008tUvTqgy1heeit6sccxj305k05kweg.jpg'
+      ],
+      nickname: 'Eren',
+      avatar: 'https://picx.zhimg.com/v2-4e92b8b1fd38e0b81bb984829293fe6a_xl.jpg?source=32738c0c',
+      contents: 'xxxxxxxxxxxxxxx'
+      },
+      {urls: [
+        'https://tvax1.sinaimg.cn/large/008tUvTqgy1heeit6sccxj305k05kweg.jpg',
+        'https://tvax1.sinaimg.cn/large/008tUvTqgy1heeit6sccxj305k05kweg.jpg',
+        'https://tvax1.sinaimg.cn/large/008tUvTqgy1heeit6sccxj305k05kweg.jpg',
+        'https://tvax1.sinaimg.cn/large/008tUvTqgy1heeit6sccxj305k05kweg.jpg'
+      ],
+      nickname: 'Mikasa',
+      avatar: 'https://picx.zhimg.com/v2-4e92b8b1fd38e0b81bb984829293fe6a_xl.jpg?source=32738c0c',
+      contents: 'xxxxxxxxxxxxxxx'
+      },
+    ],
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
--- miniprogram/page/home/index.js
+++ miniprogram/page/home/index.js
@@ -1,66 +1,35 @@
 
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
+      jumpPage(e) {
+        console.log(e.currentTarget.dataset)
+        console.log("invoke jump page")
+        wx.navigateTo({
+          url: `/page/home/${e.currentTarget.dataset.des}/index`,
+        })
+      },
+      data: {
+        pageList: [
+          {
+            key: 1,
+            title: "hobby",
+            description: "大学生交流兴趣爱好，让你与志同道合者畅所欲言。",
+            image_src: "https://tvax1.sinaimg.cn/large/008tUvTqly1heeim84enhj305k05kglu.jpg",
+            des: 'hobby'
+          },
+          {
+            key: 2,
+            title: "confess",
+            description: "大学生表白墙，倾诉心声，传递爱意，勇敢表达。让爱在校园里绽放。",
+            image_src: "https://tvax1.sinaimg.cn/large/008tUvTqgy1heeiq82vj3j305k05k3ym.jpg",
+            des: 'confess'
+          },
+          {
+            key: 3,
+            title: "sports",
+            description: "汇聚运动爱好者，分享经验、组织活动，挑战极限，激发激情，让运动成为校园生活的一部分。加入我们，追逐梦想，释放能量！",
+            image_src: "https://tvax4.sinaimg.cn/large/008tUvTqgy1heeit6sccxj305k05kweg.jpg",
+            des: 'sports'
+          }
+        ]
+      }
 })
