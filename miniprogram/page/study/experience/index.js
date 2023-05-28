97d14759f9a8c560ef00b0a9f3804b9cfcdd0bc7
Eren <347561039@qq.com>
2023-05-28 20:13:09 +0800

    finish study page framework

--- miniprogram/page/study/experience/index.js
+++ miniprogram/page/study/experience/index.js
@@ -1,11 +1,16 @@
-// page/study/experience/index.js
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+        dataList: [
+          {
+            likes: 66,
+            comments: 21,
+            nickname: 'Ackerman',
+            content: '什么都无法舍弃的人 什么都改变不了',
+            update_time: 'right now',
+            avatar: 'https://picx.zhimg.com/v2-4e92b8b1fd38e0b81bb984829293fe6a_xl.jpg?source=32738c0c'
+          }
+        ]
   },
 
   /**
--- miniprogram/page/study/index.js
+++ miniprogram/page/study/index.js
@@ -20,21 +20,21 @@ Page({
         key: 2,
         title: "交流答疑",
         description: "打破学习孤岛，大学生交流答疑学习页面，汇集学科学习方法和题目交流，共同探讨、解答，助力学业进步！",
-        image_src: "https://img2.baidu.com/it/u=778203409,525023124&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500",
+        image_src: "https://tvax4.sinaimg.cn/large/008tUvTqly1heebsiq6tbj305k05k0su.jpg",
         des: 'FAQ'
       },
       {
         key: 3,
-        title: "经验分享",
+        title: "经验",
         description: "共享智慧，启迪学习，大学生学习经验分享页面，汇聚宝贵经验，相互启发，助你走向学业成功！",
-        image_src: "https://img2.baidu.com/it/u=2951375564,2710107034&fm=253&fmt=auto&app=138&f=PNG?w=435&h=436",
+        image_src: "https://tvax1.sinaimg.cn/large/008tUvTqly1heebvo3y1mj305k05kq2z.jpg",
         des: 'experience'
       },
       {
         key: 4,
         title: "资源分享",
         description: "畅享学习，资源无限。这个页面提供电子资源分享和书籍推荐，帮助大学生们获得更多学习资料，提升成绩",
-        image_src: "https://tse3-mm.cn.bing.net/th/id/OIP-C.KDIdY2N6jOY1JCKefdI1VgHaE8?w=248&h=180&c=7&r=0&o=5&pid=1.7",
+        image_src: "https://tvax3.sinaimg.cn/large/008tUvTqly1heebwlz1xej305k05kq3b.jpg",
         des: 'share'
       }
     ]
