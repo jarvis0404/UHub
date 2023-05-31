6e246e66fb9b9840bd4e751ad72ceac88ae61161
Eren <347561039@qq.com>
2023-05-31 16:07:03 +0800

    add

--- miniprogram/page/study/FAQ/index.js
+++ miniprogram/page/study/FAQ/index.js
@@ -1,71 +1,63 @@
-
 Page({
   data: {
-        dataList: [
+        datalist: [
           {
-            likes: 66,
-            comments: 21,
-            nickname: 'Ackerman',
-            content: '什么都无法舍弃的人 什么都改变不了',
-            update_time: 'right now',
-            avatar: 'https://picx.zhimg.com/v2-4e92b8b1fd38e0b81bb984829293fe6a_xl.jpg?source=32738c0c'
+          likes: 0,
+          liked: false,
+          comments: 0,
+          nickname: 'Ackerman',
+          content: '什么都无法舍弃的人 什么都改变不了',
+          update_time: 'right now',
+          avatar: 'https://picx.zhimg.com/v2-4e92b8b1fd38e0b81bb984829293fe6a_xl.jpg?source=32738c0c',
+          urls: [
+            'https://picx.zhimg.com/v2-4e92b8b1fd38e0b81bb984829293fe6a_xl.jpg?source=32738c0c',
+            'https://picx.zhimg.com/v2-4e92b8b1fd38e0b81bb984829293fe6a_xl.jpg?source=32738c0c',
+            'https://picx.zhimg.com/v2-4e92b8b1fd38e0b81bb984829293fe6a_xl.jpg?source=32738c0c',
+            'https://picx.zhimg.com/v2-4e92b8b1fd38e0b81bb984829293fe6a_xl.jpg?source=32738c0c',
+            'https://picx.zhimg.com/v2-4e92b8b1fd38e0b81bb984829293fe6a_xl.jpg?source=32738c0c',
+            'https://picx.zhimg.com/v2-4e92b8b1fd38e0b81bb984829293fe6a_xl.jpg?source=32738c0c',
+            'https://picx.zhimg.com/v2-4e92b8b1fd38e0b81bb984829293fe6a_xl.jpg?source=32738c0c'
+          ]
           }
         ]
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
+  onLoad: function() {
+      // 加载页面后读取缓存
+      var datalist = wx.getStorageSync('FAQ_data')
+      if (datalist) {
+        this.setData({
+          datalist: datalist
+        })
+      }
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
+  save: function() {
+    // 将修改的数据更新至缓存
+    wx.setStorageSync('FAQ_data', this.data.datalist);
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+  addLike: function(e) {
+    // console.log(e.currentTarget.dataset.index)
+    // console.log(typeof e.currentTarget.dataset.index)
+    
+    var datalist = this.data.datalist;
+    
+    if (datalist)
+    {
+      // if already liked, delelte the like
+      if (datalist[e.currentTarget.dataset.index].liked == true)
+      {
+        datalist[e.currentTarget.dataset.index].likes--;
+        datalist[e.currentTarget.dataset.index].liked = false;
+        
+      }
+      else {
+        datalist[e.currentTarget.dataset.index].likes++;
+        datalist[e.currentTarget.dataset.index].liked = true;
+      }
+      // console.log(dataList[e.currentTarget.dataset.index].likes)
+      this.setData({
+        datalist: datalist
+      });
+    }
+    this.save();
   }
 })
